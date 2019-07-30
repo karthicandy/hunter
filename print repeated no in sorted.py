f7393362ade532425319c4525e4b7f69ffe2484e
karthicandy <51222427+karthicandy@users.noreply.github.com>
2019-07-30 11:46:05 +0530

    Update print repeated no in sorted.py

--- print repeated no in sorted.py
+++ print repeated no in sorted.py
@@ -2,6 +2,10 @@ k1=int(input())
 k2=list(map(int,input().split()))
 k3=[]
 for i in k2:
-  if i not in k3:
-    k3.append(i)
-print(*sorted(k3))
+  if k2.count(i)>1:
+    if i not in k3:
+      k3.append(i)
+if len(k3)>=1:
+  print(*sorted(k3))
+else:
+  print('unique')
