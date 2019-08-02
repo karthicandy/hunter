04b0aa530a31e3916e550a8f79485beaa9e32efc
karthicandy <51222427+karthicandy@users.noreply.github.com>
2019-08-02 14:22:00 +0530

    Update max sum of sub arrey.py

--- max sum of sub arrey.py
+++ max sum of sub arrey.py
@@ -1,8 +1,8 @@
-k1=int(input())
-k2=list(map(int,input().split()))[:k1]
-k3=list(map(int,(k2)))
+k2=int(input())
+k1=list(map(int,input().split()))[:k1]
+k3=list(map(int,(k1)))
 x=0
-for i in k2:
+for i in k1:
   x=x+i
   k3.append(x)
 k3.sort()
